f2ea35a89c01adeb444a9c1a81e9560d297feb5e
yuliax000 <ArthyerX@gmail.com>
2026-03-19 15:38:46 +1100

    mapping experiment example

--- mappingDesignExperiment/mappingmain.js
+++ mappingDesignExperiment/mappingmain.js
@@ -1,6 +1,9 @@
 // find our elements
 const stageContainer = document.getElementById("stage-container");
 const circleButton = document.getElementById("circleButton");
+const changeToCoral = document.getElementById("change-coral")
+const changeToYellow = document.getElementById("change-yellow")
+const changeToPink = document.getElementById("change-pink")
 // find stage width
 let stageContainerWidth = stageContainer.offsetWidth;
 console.log(stageContainerWidth);
@@ -15,10 +18,10 @@ let circleColor = "red";
 
 // create the konva stage
 const stage = new Konva.Stage({
-  container: "konva-stage",
-  width: stageContainerWidth,
-  height: stageContainerHeight,
-  fill: "blue",
+    container: "konva-stage",
+    width: stageContainerWidth,
+    height: stageContainerHeight,
+    fill: "blue",
 });
 // create layer
 const firstLayer = new Konva.Layer();
@@ -28,14 +31,33 @@ stage.add(firstLayer);
 
 //add interaction to button
 function drawNewCircle() {
-  const circle = new Konva.Circle({
-    x: stage.width() * Math.random(),
-    y: stage.height() * Math.random(),
-    radius: 50 * Math.random(),
-    fill: circleColor,
-  });
-  // add a circle to first layer,
-  firstLayer.add(circle);
+    const circle = new Konva.Circle({
+        x: stage.width() * Math.random(),
+        y: stage.height() * Math.random(),
+        radius: 50 * Math.random(),
+        fill: circleColor,
+    });
+    // add a circle to first layer,
+    firstLayer.add(circle);
 }
 
-circleButton.addEventListener("click",drawNewCircle);
+circleButton.addEventListener("click", drawNewCircle);
+
+// changing our circle color
+//I chose radio buttons because they allow for a exclusive selection, making users choice
+//limited but certain. A constricted amount of user choice leads to a tighter color palette
+// to change my color, I need to find the value of the input clicked, and then update the
+//circle color const.
+
+function changeColorRadio(clickEvent) {
+    // clickEvent.target.value;
+    // find the value of whichever of the radio buttons was clicked.
+    let newColor = clickEvent.target.value;
+    // set the new circle color to that value
+    circleColor = newColor;
+}
+
+// add addEventListeners
+changeToCoral.addEventListener("click", changeColorRadio);
+changeToYellow.addEventListener("click", changeColorRadio);
+changeToPink.addEventListener("click", changeColorRadio);
